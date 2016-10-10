74afdadbe02154b7c903c49d2276cb3f4050477b
1606149 <mebaad10@gmail.com>
2016-10-10 16:39:03 +0100

    Modification of allmovies webpage

--- allMovies.php
+++ allMovies.php
@@ -9,6 +9,28 @@
         <p>
             <?php
             print "Bouyachaka";
+
+            $db = new mysqli(
+                "us-cdbr-azure-southcentral-f.cloudapp.net",
+                "b335ab0322abab",
+                "8765cd34",
+                "database1606149"
+            );
+            if($db-> connect_errno){
+                die('Connectfailed['.$db->connect_error.']');
+            }
+
+            $sql_query = "SELECT * FROM marvelmovies";
+            $result = $db->query($sql_query);
+            while($row = $result->fetch_array()){
+                echo "<p>".$row['title'] ."<p>";
+            }
+            $result->close();
+            $db->close();
+
+
+
+
             ?>
         </p>
     </body>
