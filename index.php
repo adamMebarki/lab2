706a541e55daaa35e370cdd975b5e0961ea2988d
1606149 <mebaad10@gmail.com>
2016-10-10 14:48:44 +0100

    First example of connection on the database

--- index.php
+++ index.php
@@ -11,5 +11,45 @@
                     print "Bouyachaka";
                 ?>
             </p>
+
+    <p>
+        <?php
+
+            //1. Connect to a database server
+            //2. Choose a database
+
+            // connect to csdm webdev server and select database
+            $db = new mysqli(
+                "us-cdbr-azure-southcentral-f.cloudapp.net",
+                "b335ab0322abab",
+                "8765cd34",
+                "database1606149"
+            );
+            // test if connection was established, and print any errors
+            if($db-> connect_errno){
+                die('Connectfailed['.$db->connect_error.']');
+            }
+            //3. Execute a SQL query
+            // create a SQL query as a string
+                $sql_query = "SELECT * FROM marvelmovies WHERE title LIKE '%X%' ";
+            // execute the SQL query
+            $result = $db->query($sql_query);
+
+            //4. Process set of results for SQL query
+            // iterate over $result object one $row at a time
+            // use fetch_array() to return an associative array
+            //5. Process an individual result
+            while($row = $result->fetch_array()){
+                //process the $row
+                // print out fields from roz of data
+                echo "<p>" .$row['title'] . "<p>";
+            }
+
+            //6. Close connection to database
+            $result->close();
+            $db->close();
+        ?>
+
+    </p>
     </body>
 </html>
