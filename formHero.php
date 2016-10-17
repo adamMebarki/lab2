dd583762f0c5c3cd613fc92276270c69a360a111
1606149 <mebaad10@gmail.com>
2016-10-17 14:20:23 +0100

    lab 3 create a heroform

--- formHero.php
+++ formHero.php
@@ -10,7 +10,7 @@
             <form action="formResponse.php" method="post">
                 <label>First Name</label>
                     <input type=“text” name=“firstname”>
-                <label>Last Name</label>
+                <!--<label>Last Name</label>
                     <input type=“text” name=“lastname”>
                 <br/>
                 <label>Gender</label><br/>
@@ -18,7 +18,7 @@
                     <input type="radio" name="gender" value="female"> Female<br>
                 <br/>
                 <label>Main Superhero Power</label>
-                    <input type=“text” name=“mainpower”>
+                    <input type=“text” name=“mainpower”>-->
                 <input type="submit" value="Submit">
             </form>
         </p>
