b4cfc89eadfce74fedcef0a750f4cbf5e7264d53
1606149 <mebaad10@gmail.com>
2016-10-17 13:37:10 +0100

    Lab3 hello planet

--- helloPrinter.php
+++ helloPrinter.php
@@ -8,7 +8,7 @@
         <p>
             <?php
                 $planet = $_GET["planet"];
-               echo "<p>Hello".$planet."</p>";
+               echo "<p>Hello <b>".$planet."</b></p>";
             ?>
         </p>
 
--- helloSelector.php
+++ helloSelector.php
@@ -9,9 +9,9 @@
             <br>LAB TWO<br>
             Where would you like to say hello ?
         <ul>
-            <li><a href="helloPrinter.php?planet=earth">Earth</a></li>
-            <li><a href="helloPrinter.php?planet=mars">Mars</a></li>
-            <li><a href="helloPrinter.php?planet=uranus">Uranus</a></li>
+            <li><a href="helloPrinter.php?planet=Earth">Earth</a></li>
+            <li><a href="helloPrinter.php?planet=Mars">Mars</a></li>
+            <li><a href="helloPrinter.php?planet=Uranus">Uranus</a></li>
         </ul>
         </p>
     
