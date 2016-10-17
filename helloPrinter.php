1269ebf7eb23d3cee82497273beaeeb92e8c5aa7
1606149 <mebaad10@gmail.com>
2016-10-17 13:35:08 +0100

    Lab3 hello planet

--- helloPrinter.php
+++ helloPrinter.php
@@ -8,7 +8,7 @@
         <p>
             <?php
                 $planet = $_GET["planet"];
-               echo "<p>Hello .$planet.</p>";
+               echo "<p>Hello".$planet."</p>";
             ?>
         </p>
 
