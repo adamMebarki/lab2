38fa827b48a5df1762a09e43aa9833f40905b96d
1606149 <mebaad10@gmail.com>
2016-10-17 14:22:40 +0100

    lab 3 create a heroform

--- formHero.php
+++ formHero.php
@@ -9,16 +9,16 @@
             Welcome new hero please fill the following form :
             <form action="formResponse.php" method="post">
                 <label>First Name</label>
-                    <input type=“text” name="firstname">
-                <!--<label>Last Name</label>
-                    <input type=“text” name=“lastname”>
+                    <input type="text" name="firstname">
+                <label>Last Name</label>
+                    <input type="text" name="lastname">
                 <br/>
                 <label>Gender</label><br/>
                     <input type="radio" name="gender" value="male"> Male<br>
                     <input type="radio" name="gender" value="female"> Female<br>
                 <br/>
                 <label>Main Superhero Power</label>
-                    <input type=“text” name=“mainpower”>-->
+                    <input type="text" name="mainpower">
                 <input type="submit" value="Submit">
             </form>
         </p>
