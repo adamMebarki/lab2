6cb826449c35433eb15528c2430172c9b464999f
1606149 <mebaad10@gmail.com>
2016-10-17 13:15:46 +0100

    Modification of every webpage of lab2

--- marvelStudios.php
+++ marvelStudios.php
@@ -9,6 +9,23 @@
         <p>
             <?php
             print "Bouyachaka";
+            $db = new mysqli(
+                "us-cdbr-azure-southcentral-f.cloudapp.net",
+                "b335ab0322abab",
+                "8765cd34",
+                "database1606149"
+            );
+            if($db-> connect_errno){
+                die('Connectfailed['.$db->connect_error.']');
+            }
+
+            $sql_query = "SELECT * FROM marvelmovies WHERE productionStudio LIKE 'Marvel Studios'";
+            $result = $db->query($sql_query);
+            while($row = $result->fetch_array()){
+                echo "<p>".$row['title']. $row['yearReleased']. $row['productionStudio']. $row['notes']."<p>";
+            }
+            $result->close();
+            $db->close();
             ?>
         </p>
     </body>
